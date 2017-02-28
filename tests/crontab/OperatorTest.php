195875d72c136dcc5f8e6435bb4f6b37779c0cec
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2017-02-28 23:01:03 +0100

    Dummy operator test

---
+++ tests/crontab/OperatorTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of SebastianFeldmann\Crontab.
+ *
+ * (c) Sebastian Feldmann <sf@sebastian-feldmann.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace SebastianFeldmann\Crontab;
+
+/**
+ * Class OperatorTest
+ *
+ * @package SebastianFeldmann\Crontab
+ */
+class OperatorTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Tests Operator::getJobs
+     */
+    public function _testDefault()
+    {
+        $this->assertTrue(true);
+    }
+}
