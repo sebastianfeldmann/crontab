6320e6fa3752ca6e76954d7ad0018e695299cd20
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2017-02-28 19:57:30 +0100

    Add Job value object

---
+++ src/Job.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * This file is part of SebastianFeldmann\Crontab.
+ *
+ * (c) Sebastian Feldmann <sf@sebastian-feldmann.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace SebastianFeldmann\Crontab;
+
+/**
+ * Class Job
+ *
+ * @package SebastianFeldmann\Crontab
+ * @author  Sebastian Feldmann <sf@sebastian-feldmann.info>
+ * @link    https://github.com/sebastianfeldmann/crontab
+ * @since   Class available since Release 0.9.0
+ */
+class Job
+{
+    /**
+     * Cron schedule expression.
+     *
+     * @var string
+     */
+    private $schedule;
+
+    /**
+     * Command that should get executed.
+     *
+     * @var string
+     */
+    private $command;
+
+    /**
+     * List of comments to describe the command.
+     *
+     * @var array
+     */
+    private $comments;
+
+    /**
+     * Entry constructor.
+     *
+     * @param string $schedule
+     * @param string $command
+     * @param array  $comments
+     */
+    public function __construct(string $schedule, string $command, array $comments = [])
+    {
+        $this->schedule = $schedule;
+        $this->command  = $command;
+        $this->comments = $comments;
+    }
+
+    public function getSchedule() : string
+    {
+        return $this->schedule;
+    }
+
+    /**
+     * Return the command executed by the crontab entry.
+     *
+     * @return string
+     */
+    public function getCommand() : string
+    {
+        return $this->command;
+    }
+
+    /**
+     * Return comment list.
+     *
+     * @return array
+     */
+    public function getComments() : array
+    {
+        return $this->comments;
+    }
+
+    /**
+     * Return comments as string.
+     * All lines get prefixed with '# '
+     *
+     * @return string
+     */
+    public function formatComments() : string
+    {
+        $doc = '';
+        foreach ($this->comments as $comment) {
+            $doc .=   '# ' . $comment . PHP_EOL;
+        }
+        return $doc;
+    }
+
+    /**
+     * Magic to string method.
+     *
+     * @return string
+     */
+    public function __toString() : string
+    {
+        return $this->formatComments() . $this->schedule . ' ' . $this->command . PHP_EOL;
+    }
+}
---
+++ tests/crontab/JobTest.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * This file is part of SebastianFeldmann\Crontab.
+ *
+ * (c) Sebastian Feldmann <sf@sebastian-feldmann.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace SebastianFeldmann\Crontab;
+
+/**
+ * Class JobTest
+ *
+ * @package SebastianFeldmann\Crontab
+ */
+class JobTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Tests Job::getSchedule
+     */
+    public function testGetSchedule()
+    {
+        $job = new Job('* * * * *', 'echo 1');
+        $this->assertEquals('* * * * *', $job->getSchedule());
+    }
+
+    /**
+     * Tests Job::getCommand
+     */
+    public function testGetCommand()
+    {
+        $job = new Job('* * * * *', 'echo 1');
+        $this->assertEquals('echo 1', $job->getCommand());
+    }
+
+    /**
+     * Tests Job::getComments
+     */
+    public function testGetComments()
+    {
+        $job = new Job('* * * * *', 'echo 1', ['foo']);
+        $this->assertEquals(['foo'], $job->getComments());
+    }
+
+
+    /**
+     * Tests Job::__toString
+     */
+    public function testGetToString()
+    {
+        $job = new Job('* * * * *', 'echo 1', ['foo']);
+        $this->assertEquals(
+            '# foo' . PHP_EOL . '* * * * * echo 1' . PHP_EOL,
+            (string) $job
+        );
+    }
+}
